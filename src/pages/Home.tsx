44b21e0fe27ca84a32bc287f6f618ba85d52bf03
Fernando Marca Magalhães <fernandomarca@hotmail.com>
2022-02-27 18:10:49 -0300

    Revert "new feature edit task"
    This reverts commit 441ca7c4aae29316d7b035cebb504e909b6eda3c.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,41 +1,33 @@
 import React, { useState } from 'react';
-import { Alert, StyleSheet, View, AlertButton } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 import { Header } from '../components/Header';
 import { Task, TasksList } from '../components/TasksList';
 import { TodoInput } from '../components/TodoInput';
 
-export type EditTaskArgs = {
-  taskId: number,
-  taskNewTitle: string
-}
-
 export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    const findTask = tasks.find((task) => task.title === newTaskTitle);
-    if (findTask) {
-      return Alert.alert("Task já cadastrada", "Você não pode cadastrar uma task com o mesmo nome");
-    }
-    setTasks((old) => [...old,
-    {
-      id: new Date().getTime(),
-      title: newTaskTitle,
-      done: false
+    setTasks((old)=>[...old,
+      {
+      id:new Date().getTime(),
+      title:newTaskTitle,
+      done:false
     }]);
   }
 
   function handleToggleTaskDone(id: number) {
-    const findTask = tasks.find((task) => task.id === id);
-    if (!findTask) {
+    //TODO - toggle task done if exists
+    const findTask = tasks.find((task)=>task.id === id);
+    if(!findTask){
       return
     }
-    setTasks((old) => old.map((task) => {
-      if (task.id === id) {
+    setTasks((old)=>old.map((task)=>{
+      if(task.id === id){
         return {
           ...findTask,
-          done: !findTask.done
+          done:!findTask.done
         }
       }
       return task;
@@ -43,30 +35,10 @@ export function Home() {
   }
 
   function handleRemoveTask(id: number) {
-    Alert.alert("Remover item", "Tem certeza que você deseja remover esse item?", [
-      {
-        style: "cancel",
-        text: "Não"
-      },
-      {
-        style: "destructive",
-        text: "Sim",
-        onPress: () => {
-          setTasks((old) => old.filter((item) => item.id !== id));
-        }
-      }
-    ])
-  }
-
-  function handleEditTask({ taskId, taskNewTitle }: EditTaskArgs) {
-    const updateTasks = tasks.map(task => ({ ...task }));
-    const task = updateTasks.find(task => task.id === taskId);
-    if (!task) {
-      return;
-    }
-
-    task.title = taskNewTitle;
-    setTasks(updateTasks);
+    //TODO - remove task from state
+    // const filteredTasks = tasks.filter((item)=>item.id !== id);
+    // setTasks(filteredTasks);
+    setTasks((old)=>old.filter((item)=>item.id !== id));
   }
 
   return (
@@ -75,11 +47,10 @@ export function Home() {
 
       <TodoInput addTask={handleAddTask} />
 
-      <TasksList
-        tasks={tasks}
+      <TasksList 
+        tasks={tasks} 
         toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask}
-        editTask={handleEditTask}
+        removeTask={handleRemoveTask} 
       />
     </View>
   )

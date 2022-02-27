86e859f77823bb66fdba1d8a02c5167f511bde6c
Fernando Marca Magalhães <fernandomarca@hotmail.com>
2022-02-27 18:15:47 -0300

    revert

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,25 +9,25 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    setTasks((old)=>[...old,
-      {
-      id:new Date().getTime(),
-      title:newTaskTitle,
-      done:false
+    setTasks((old) => [...old,
+    {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false
     }]);
   }
 
   function handleToggleTaskDone(id: number) {
     //TODO - toggle task done if exists
-    const findTask = tasks.find((task)=>task.id === id);
-    if(!findTask){
+    const findTask = tasks.find((task) => task.id === id);
+    if (!findTask) {
       return
     }
-    setTasks((old)=>old.map((task)=>{
-      if(task.id === id){
+    setTasks((old) => old.map((task) => {
+      if (task.id === id) {
         return {
           ...findTask,
-          done:!findTask.done
+          done: !findTask.done
         }
       }
       return task;
@@ -35,10 +35,7 @@ export function Home() {
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
-    // const filteredTasks = tasks.filter((item)=>item.id !== id);
-    // setTasks(filteredTasks);
-    setTasks((old)=>old.filter((item)=>item.id !== id));
+    setTasks((old) => old.filter((item) => item.id !== id));
   }
 
   return (
@@ -47,10 +44,10 @@ export function Home() {
 
       <TodoInput addTask={handleAddTask} />
 
-      <TasksList 
-        tasks={tasks} 
+      <TasksList
+        tasks={tasks}
         toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask} 
+        removeTask={handleRemoveTask}
       />
     </View>
   )
